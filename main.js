23cce6cc9cc389409fcb71b6dc626315b08f7485
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-02-12 21:51:07 -0300

    Passando parametros na requisição

--- main.js
+++ main.js
@@ -5,11 +5,18 @@
 // .then((data) => console.log(data))
 
 //ultilizando o fatch com async/await
-
+//faz a mesma coisa que o fatch .then
 async function fetchProducts() {
     const response = await fetch("http://localhost:3333/products")
     const data = await response.json()
     console.log(data)
 }
 
-fetchProducts()
+//Temos aqui uma função que esta buscando por um produto especifico 
+async function fetchProductById(id){
+    const response = await fetch(`http://localhost:3333/products/${id}`)
+    const data = await response.json()
+    console.log(data)
+}
+// fetchProducts()
+fetchProductById("3")
