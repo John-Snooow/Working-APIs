97ffeac293fca6f3f4c57ae7363fa3a19ef9d078
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-02-12 21:42:41 -0300

    ultilizando o fatch com async/await

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-fetch("http://localhost:3333/products")
-.then((response) => response.json())//pasndo a respota para jsons
-.then((data) => console.log(data))
+//ultilizando o fatch do o then
+
+// fetch("http://localhost:3333/products")
+// .then((response) => response.json())//pasndo a respota para jsons
+// .then((data) => console.log(data))
+
+//ultilizando o fatch com async/await
+
+async function fetchProducts() {
+    const response = await fetch("http://localhost:3333/products")
+    const data = await response.json()
+    console.log(data)
+}
+
+fetchProducts()
